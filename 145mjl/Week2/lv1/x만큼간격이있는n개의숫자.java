f21159a11da6cfb85b3678f064c85f8a6ae22481
145mjl <113668785+145mjl@users.noreply.github.com>
2023-10-19 16:43:13 +0900

    Create x만큼간격이있는n개의숫자.java

---
+++ 145mjl/Week2/lv1/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long element=x;
+        for(int i=0;i<n;i++){
+            answer[i]=element*(i+1);
+        }
+        return answer;
+    }
+}

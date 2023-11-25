6bf2cc3578f275c430400c3c03f696e42e0cf1bd
145mjl <113668785+145mjl@users.noreply.github.com>
2023-11-25 17:17:03 +0900

    Week8 업데이트

---
+++ 145mjl/Week8/추억점수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for(int i=0;i<photo.length;i++){
+            for(int j=0;j<photo[i].length;j++){
+                int index=-1;
+                for(int k=0;k<name.length;k++){
+                    if(name[k].equals(photo[i][j])){
+                        index=k;
+                    }
+                }
+                if(index!=-1){
+                    answer[i]+=yearning[index];
+                }
+            }
+        }
+        return answer;
+    }
+}

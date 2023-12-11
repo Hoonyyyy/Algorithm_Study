608302a6499d0da5e5c11e192ef2bdc0c885aacc
145mjl <113668785+145mjl@users.noreply.github.com>
2023-12-11 17:39:27 +0900

    Update 모의고사.java

--- 145mjl/Week9/모의고사.java
+++ 145mjl/Week9/모의고사.java
@@ -1,39 +1,57 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
-        //12345,21232425,3311224455
-        int[] pattern1=new int[]{1,2,3,4,5};
-        int[] pattern2=new int[]{2,1,2,3,2,4,2,5};
-        int[] pattern3=new int[]{3,3,1,1,2,2,4,4,5,5};
-        int score1=countScore(answers,pattern1);
-        int score2=countScore(answers,pattern2);
-        int score3=countScore(answers,pattern3);
+        Integer[] pattern1=new Integer[]{1,2,3,4,5};
+        Integer[] pattern2=new Integer[]{2,1,2,3,2,4,2,5};
+        Integer[] pattern3=new Integer[]{3,3,1,1,2,2,4,4,5,5};
+        
+        int score1=check(Arrays.asList(pattern1),answers);
+        int score2=check(Arrays.asList(pattern2),answers);
+        int score3=check(Arrays.asList(pattern3),answers);
 
-        System.out.println(score1);
-        System.out.println(score2);
-        System.out.println(score3);
+        answer=highestScorers(new int[]{score1,score2,score3});
+        
         return answer;
     }
     
-    public int countScore(int[] answers,int[] pattern){
+    public int check(List<Integer> list,int[] arr){//채점
         int score=0;
-        if(answers.length>=pattern.length){
-            for(int i=0;i<answers.length;i+=pattern.length){
-                for(int j=i;j<pattern.length;j++){
-                    if(answers[j]==pattern[j]){
-                        score+=1;
-                    }
-                }
+        for(int i=0,index=0;i<arr.length;i++,index++){ 
+            
+            if(index==list.size()){
+                index=0;
             }
-        }else{
-            for(int i=0;i<answers.length;i++){
-                if(answers[i]==pattern[i]){
-                    score+=1;
-                }
+            if(arr[i]==list.get(index)){            
+                score++;
             }
         }
         
-
         return score;
     }
+    
+    public int[] highestScorers(int[] scores){//최고 득점자 구하여 내림차순 정렬
+        
+        List<Integer> list=new ArrayList<>();
+        int[] keys={1,2,3};
+        int max=0;
+        
+        for(int i=0;i<scores.length;i++){
+            if(max<scores[i]){
+                max=scores[i];
+            }
+        }
+        
+        for(int i=0;i<scores.length;i++){
+            if(scores[i]==max){
+                list.add(keys[i]);
+            }
+        }
+        
+        Collections.sort(list);
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
 }
